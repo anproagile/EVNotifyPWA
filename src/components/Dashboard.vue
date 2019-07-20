135e807c1823977fafa5e7358b16316c468ca482
anproagile <anproagile@gmail.com>
2019-07-20 22:07:52 +0200

    Added modal for SOC explaination.

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,14 +1,15 @@
 <template>
   <v-layout>
-    <v-dialog v-model="showBtnNotImplementedExplaination" max-width="290" persistent scrollable>
+    <v-dialog v-model="showSOCExplaination" max-width="290" persistent scrollable>
       <v-card>
-        <v-card-title class="headline">This value is empty?</v-card-title>
+        <v-card-title class="headline">State of charge</v-card-title>
         <v-card-text>
-          For users with EvNotiPi / Plug'n'Play devices this value is not implemented at the moment. But it will follow soon, don´t worry.<br><br>
+          Display: {{ syncData.soc_display }} % (displayed value in car) <br/>
+          BMS: {{ syncData.soc_bms }} % (calculated value by system)
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="green darken-1" flat @click="showBtnNotImplementedExplaination = false">Close</v-btn>
+          <v-btn color="green darken-1" flat @click="showSOCExplaination = false">Close</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -20,13 +21,14 @@
               <v-progress-circular :rotate="-90" :size="100" :width="15"
                 :value="syncData.soc_display || syncData.soc_bms" :color="cycleColor">
                 <div class="progress-cycle-text-container">
-                  <p>{{ syncData.soc_display ||syncData.soc_bms }} %</p>
+                  <p>{{ syncData.soc_display || syncData.soc_bms }} %</p>
                   <v-icon color="primary" v-if="syncData.charging">flash_on</v-icon>
                 </div>
               </v-progress-circular>
-              <p class="caption progress-cycle-text" v-if="syncData.soc_display">SOC Display</p>
-              <p class="caption progress-cycle-text" v-else>SOC BMS</p>
             </div>
+            <v-btn class="socexplainationmodal" icon ripple @click="showSOCExplaination = true">
+              <v-icon color="grey lighten-1">info</v-icon>
+            </v-btn>
             <div class="progress-cycle-container right">
               <v-list>
                 <v-list-tile>
@@ -113,22 +115,8 @@
                   <v-list-tile-title>{{ syncData.cumulative_energy_charged || 0 }} kWh / {{ syncData.cumulative_energy_discharged || 0 }} kWh </v-list-tile-title>
                   <v-list-tile-sub-title>Cumulative energy charged / <br> Cumulative energy discharged</v-list-tile-sub-title>
                 </v-list-tile-content>
-                <v-list-tile-action>
-                  <v-btn icon ripple @click="showBtnNotImplementedExplaination = true">
-                    <v-icon color="grey lighten-1">info</v-icon>
-                  </v-btn>
-                </v-list-tile-action>
               </v-list-tile>
               <v-subheader>Battery health</v-subheader>
-              <v-list-tile v-if="syncData.soc_display">
-                <v-list-tile-action>
-                  <v-icon color="teal">battery_std</v-icon>
-                </v-list-tile-action>
-                <v-list-tile-content>
-                  <v-list-tile-title>{{ syncData.soc_bms || 0 }} %</v-list-tile-title>
-                  <v-list-tile-sub-title>SOC BMS</v-list-tile-sub-title>
-                </v-list-tile-content>
-              </v-list-tile>
               <v-list-tile>
                 <v-list-tile-action>
                   <v-icon color="teal">favorite</v-icon>
@@ -185,7 +173,7 @@ import { setInterval } from 'timers';
       dataOutdatedMessage: '',
       dataOutdatedMessageTimestamp: '',
       settings: storage.getValue('settings', {}),
-      showBtnNotImplementedExplaination: false
+      showSOCExplaination: false
     }),
     computed: {
       cycleColor() {
@@ -339,6 +327,10 @@ import { setInterval } from 'timers';
   }
   .v-subheader {
     height: 20px;
+  }
+  .socexplainationmodal {
+    position: absolute;
+    margin-left: -5px;
   }
 </style>
 
